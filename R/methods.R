17f104ee0f07b988509068d6625fb96056d2e76e
Deerspring 鹿泉 <meucci.ilunga@gmail.com>
2025-01-31 18:12:32 -0500

    added skeleton of three core hiperglm methods: print, coef, vcov

---
+++ R/methods.R
@@ -0,0 +1,23 @@
+# Coefficients
+#' Extract coefficients from a hiperglm object
+#' @export
+coef.hiperglm <- function(hiperglm_obj) {
+  warning("coef.hiperglm is not yet implemented.")
+  return(NULL)
+}
+
+# Covariance Matrix
+#' Extract covariance matrix from a hiperglm object
+#' @export
+vcov.hiperglm <- function(hiperglm_obj) {
+  warning("vcov.hiperglm is not yet implemented.")
+  return(NULL)
+}
+
+#' Summarize Model
+#' Print method for a hiperglm object
+#' @export
+print.hiperglm <- function(hiperglm_obj) {
+  warning("print.hiperglm is not yet implemented.")
+  invisible(x)
+}
